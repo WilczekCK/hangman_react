20901282513871ff9ccca3afc4e651ffc8f8338e
Paweł Wilk <pwilk0108@gmail.com>
2022-10-06 16:55:40 +0200

    made the code more beauty

--- src/layouts/LettersPanel.js
+++ src/layouts/LettersPanel.js
@@ -2,31 +2,31 @@ import React from 'react';
 import LettersDisplay from '../components/LettersDisplay';
 import LetterInput from '../components/LetterInput';
 
-
 class LettersPanel extends React.Component{
     constructor(props){
         super(props);
 
-        this.state = {
-            wrongLetters: '',
-            properLetters: ''
-        }
+        this.state = { wrongLetters: '', properLetters: '' }
         this.onLetterChange = this.onLetterChange.bind(this);
     }
 
     onLetterChange(e){
-        this.setState({
-            wrongLetters: e.wrongLetters,
-            properLetters: e.properLetters
-        })
+        this.setState({ wrongLetters: e.wrongLetters, properLetters: e.properLetters })
     }
-
 
     render(){
         return (
             <div>
-                <LettersDisplay properLetters={this.state.properLetters} wrongLetters={this.state.wrongLetters} wordToGuess='Kolorowe Korale Królowej Karoliny'/>
-                <LetterInput onLetterChange={this.onLetterChange} wordToGuess={ this.props.wordToGuess.toLowerCase() } />
+                <LettersDisplay 
+                    properLetters={this.state.properLetters} 
+                    wrongLetters={this.state.wrongLetters}
+                    wordToGuess='Kolorowe Korale Królowej Karoliny'
+                />
+                
+                <LetterInput 
+                    onLetterChange={this.onLetterChange}
+                    wordToGuess={this.props.wordToGuess.toLowerCase()} 
+                />
             </div>
         )
     }
