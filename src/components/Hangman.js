68495eb47077ab5c53e8b786b47d2c5daf409e9b
Paweł Wilk <pwilk0108@gmail.com>
2022-10-14 15:51:56 +0200

    created a component

---
+++ src/components/Hangman.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hangman extends React.Component{
+    constructor(props){
+        super();
+    }
+
+    render(){
+        return '';
+    }
+}
+
+export default Hangman;
