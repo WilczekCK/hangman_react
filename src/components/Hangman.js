18d24c8b125ecdf6782d657fc2631e040db6329d
Paweł Wilk <pwilk0108@gmail.com>
2022-10-14 16:22:53 +0200

    head and line with table appended, the start of css

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -3,10 +3,43 @@ import React from 'react';
 class Hangman extends React.Component{
     constructor(props){
         super();
+
+        this.createTable = this.createTable.bind(this);
+    }
+
+    createTable(){
+        return (
+            <table>
+                <tr>
+                    <td></td>
+                    <td class="hangman__line--horizontal"></td>
+                    <td class="hangman__line--head"></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td class="hangman--head"></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        )
     }
 
     render(){
-        return '';
+        return this.createTable();
     }
 }
 
