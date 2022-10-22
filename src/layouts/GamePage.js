edaf7746011bc5920fd4018352df754bf9306092
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-10-23 00:35:22 +0200

    className instead of class

--- src/layouts/GamePage.js
+++ src/layouts/GamePage.js
@@ -14,18 +14,18 @@ class GamePage extends React.Component{
                     <h2> Player points </h2>
 
                     <div className="page__container__points">
-                        <div class="page__container__points__heading">
+                        <div className="page__container__points__heading">
                             <ul>
                                 <li>Player 1</li>
                                 <li>Player 2</li>
                             </ul>
                         </div>
                         
-                        <div class="page__container__points__players">
-                            <div class="page__container__points__players--a">
+                        <div className="page__container__points__players">
+                            <div className="page__container__points__players--a">
                                 4
                             </div>
-                            <div class="page__container__points__players--b">
+                            <div className="page__container__points__players--b">
                                 2
                             </div>
                         </div>
@@ -34,6 +34,8 @@ class GamePage extends React.Component{
                 </div>
 
                 <div className="page__container page__container--right">
+                    <h2> Player guessing... </h2>
+                    
                     <Hangman healthLeft={this.props.healthLeft} maxMistakes={this.props.maxMistakes}/>
                     <LettersPanel onWrongLetter={this.props.onWrongLetter} wordToGuess={this.props.wordToGuess}/>
                 </div>
