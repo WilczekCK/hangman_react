35b04864374cd931c236ac6efce302c581c7e6f9
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-10-22 23:58:46 +0200

    player points table created

--- src/layouts/GamePage.js
+++ src/layouts/GamePage.js
@@ -10,7 +10,35 @@ class GamePage extends React.Component{
     render(){
         return (
             <div className={this.props.classList}>
-                <div className="page__container page__container--left"></div>
+                <div className="page__container page__container--left">
+                    <h2> Player points </h2>
+
+                    <div className="page__container__points">
+                        <div class="page__container__points__heading">
+                            <ul>
+                                <li>Player 1</li>
+                                <li>Player 2</li>
+                            </ul>
+                        </div>
+                        
+                        <div class="page__container__points__players">
+                            <div class="page__container__points__players--a">
+                                <ul>
+                                    <li>4</li>
+                                    <li>In progress...</li>
+                                </ul>
+                            </div>
+                            <div class="page__container__points__players--b">
+                                <ul>
+                                    <li>2</li>
+                                    <li>In progress...</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
                 <div className="page__container page__container--right">
                     <Hangman healthLeft={this.props.healthLeft} maxMistakes={this.props.maxMistakes}/>
                     <LettersPanel onWrongLetter={this.props.onWrongLetter} wordToGuess={this.props.wordToGuess}/>
