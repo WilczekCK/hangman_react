4736ee6e58fa0e6a1dfa59eb63205e17606b8b34
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-10-22 14:54:18 +0200

    it changes the screen if the player is dead

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component{
   constructor(){
     super();
     const maxAttempts = 5;
-    this.state = {healthAmount: maxAttempts, maxMistakes: maxAttempts, actualScreen: 'start'};
+    this.state = {healthAmount: maxAttempts, maxMistakes: maxAttempts, actualScreen: 'game'};
     this.dropHealth = this.dropHealth.bind(this);
   }
   
@@ -16,6 +16,14 @@ class App extends React.Component{
     this.setState((state, props) => ({
       healthAmount: state.healthAmount - 1
     }));
+
+    if( this.state.healthAmount <= 1 ) {
+      this.setState({actualScreen: 'word_select'})
+    } 
+  }
+
+  isPlayerStillAlive() {
+    
   }
 
   render(){
@@ -28,8 +36,8 @@ class App extends React.Component{
           <LettersPanel onWrongLetter={this.dropHealth} wordToGuess='Madagaskar'/>
         </div>
 
-        <div className={ this.state.actualScreen === 'word_select_first' ? 'word_select_start visible' : 'start' }></div>
-        <div className={ this.state.actualScreen === 'word_select' ? 'word_select visible' : 'word_select' } ></div>
+        <div className={ this.state.actualScreen === 'word_select_first' ? 'word_select_start visible' : 'start' }> Wpisz hasło, pierwsza osoba. </div>
+        <div className={ this.state.actualScreen === 'word_select' ? 'word_select visible' : 'word_select' } > Wpisz hasło, nie pierwsza osoba. </div>
       </div>
     );
   }
