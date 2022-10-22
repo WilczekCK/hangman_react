ac5ddb0d892c51acd83538eca2238d336ace107b
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-10-22 14:42:30 +0200

    the screen changes depends of state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component{
   constructor(){
     super();
     const maxAttempts = 5;
-    this.state = {healthAmount: maxAttempts, maxMistakes: maxAttempts};
+    this.state = {healthAmount: maxAttempts, maxMistakes: maxAttempts, actualScreen: 'start'};
     this.dropHealth = this.dropHealth.bind(this);
   }
   
@@ -21,15 +21,15 @@ class App extends React.Component{
   render(){
     return (
       <div className="App">
-        <div className="start"></div>
+        <div className={ this.state.actualScreen === 'start' ? 'start visible' : 'start' }></div>
 
-        <div className="game">
+        <div className={ this.state.actualScreen === 'game' ? 'game visible' : 'game' }>
           <Hangman healthLeft={this.state.healthAmount} maxMistakes={this.state.maxMistakes}/>
           <LettersPanel onWrongLetter={this.dropHealth} wordToGuess='Madagaskar'/>
         </div>
 
-        <div className="word_select_start"></div>
-        <div className="word_select_another"></div>
+        <div className={ this.state.actualScreen === 'word_select_first' ? 'word_select_start visible' : 'start' }></div>
+        <div className={ this.state.actualScreen === 'word_select' ? 'word_select visible' : 'word_select' } ></div>
       </div>
     );
   }
