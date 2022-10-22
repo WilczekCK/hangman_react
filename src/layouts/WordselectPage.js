55db3dda285b47a2781b8d1758fe9249d3e6ae30
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-10-22 22:04:07 +0200

    added images of divider and bg of paper

--- src/layouts/WordselectPage.js
+++ src/layouts/WordselectPage.js
@@ -29,13 +29,12 @@ class WordselectPage extends React.Component{
             <div className={this.props.classList}>
                 <div data-firstPage={this.props.previousScreen === 'start'} className="page__container page__container--left"></div>
                 <div className="page__container page__container--right">
-                    <h2> Select password </h2>
+                    <h2> Type the word to guess: </h2>
                     <form onSubmit={(e) => e.preventDefault()}>
                         <input type="text" name="maxMistakes" onChange={this.handleInputChange} value={this.state.wordToGuess} placeholder="Madagascar"/>
                     </form>
 
-                    <br/>
-                    <button disabled={!this.state.isWordProvided}  data-goto="game" data-wordtoguess={this.state.wordToGuess} onClick={ this.props.changeScreen }>Play</button>
+                    <div className="page__container__triangle" disabled={!this.state.isWordProvided}  data-goto="game" data-wordtoguess={this.state.wordToGuess} onClick={ this.props.changeScreen }>PLAY</div>
                 </div>
             </div>
         );
