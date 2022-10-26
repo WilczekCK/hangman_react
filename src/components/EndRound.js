39572d9034486c546c8a0e59ec03c7de8603890a
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-10-26 16:43:05 +0200

    css codes to highlight win or lose string

--- src/components/EndRound.js
+++ src/components/EndRound.js
@@ -9,7 +9,7 @@ class EndRound extends React.Component{
         return(
             <div className={!this.props.isHangmanAlive || this.props.isWordGuessed ? 'letters__container__input' : 'letters__container__input hide'}>
                 <div className="page__container__triangle" data-goto="word_select" onClick={(e) => {this.props.changeScreen(e); this.props.clearLetterPanel(e)} }>PLAY</div>
-                {this.props.isWordGuessed ? 'You win!': "You lose!"}
+                {this.props.isWordGuessed ? <h2 className="letters__container__endround--win">You win!</h2>: <h2 className="letters__container__endround--lose">You lose!</h2>}
             </div>
         )
     }
